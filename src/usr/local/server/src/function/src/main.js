aa46d43b6750f375586dc0786b217f948b73c9a6
choir27 <richardchoi54@gmail.com>
2024-07-12 10:23:21 -0400

    test2

--- src/usr/local/server/src/function/src/main.js
+++ src/usr/local/server/src/function/src/main.js
@@ -12,7 +12,7 @@ export default async ({ req, res, log, error }) => {
     if (req.method === 'GET') {
       // Send a response with the res object helpers
       // `res.send()` dispatches a string back to the client
-      return res.send('Hello, World!');
+      return res.text('Hello, World!');
     }
   
     // `res.json()` is a handy helper for sending JSON
