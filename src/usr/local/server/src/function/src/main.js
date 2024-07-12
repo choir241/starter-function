f9b473d87b40d75be8d8b63b9a0f26de78b39332
choir27 <richardchoi54@gmail.com>
2024-07-12 11:16:06 -0400

    initialize appwrite client to test dynamic api keys

--- src/usr/local/server/src/function/src/main.js
+++ src/usr/local/server/src/function/src/main.js
@@ -1,13 +1,18 @@
+const appwrite = require("node-appwrite")
 // This is your Appwrite function
 // It's executed each time we get a request
 export default async ({ req, res, log, error }) => {
 
-    log(error);
+ appwriteClient = client.NewClient()
+	appwriteClient.SetEndpoint(os.Getenv("https://v16.appwrite.org/v1"))
+	appwriteClient.SetProject(os.Getenv("APPWRITE_FUNCTION_PROJECT_ID"))
+	appwriteClient.SetKey(Context.Req.Headers["x-appwrite-key"])
 
-    if (req.method === 'POST') {
-      log(req.bodyJson);
-      log(req.bodyText)
-      return req.bodyText;
-    }
-  
-  };
+
+  if (req.method === 'GET') {
+    const database = appwrite.Databases(appwriteClient);
+    log(database.listDocuments('database','collection'));
+    return(database.listDocuments);
+  }
+
+};
