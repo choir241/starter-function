02dcaea8f373fe93a6de1d81de8dea88d33d8f2e
choir27 <richardchoi54@gmail.com>
2024-07-12 13:08:40 -0400

    update test 6

--- src/usr/local/server/src/function/src/main.js
+++ src/usr/local/server/src/function/src/main.js
@@ -1,21 +1,14 @@
-import { Databases, Client } from "node-appwrite";
+import { Users, Client } from 'node-appwrite';
 // This is your Appwrite function
 // It's executed each time we get a request
 export default async ({ req, res, log, error }) => {
-
- const appwriteClient = new Client()
-    .setEndpoint("https://v16.appwrite.org/v1")
-    .setProject("668840fe0017793b93a6")
-    .setKey(req.headers["x-appwrite-key"])
-;
-
-
   if (req.method === 'GET') {
-    const database = new Databases(appwriteClient);
-    const documents = await database.listDocuments('66884220000662def5a4','66884223000a7bff327a');
-
-    log(documents.documents);
-    return database.listDocuments;
+    const client = new Client()
+      .setEndpoint('https://v16.appwrite.org/v1')
+      .setProject('668840fe0017793b93a6')
+      .setKey(req.headers['x-appwrite-key']);
+    const users = new Users(client);
+    const user = await users.create(ID.unique(), 'matej@appwrite.io');
+    return res.send(user.$id);
   }
-
 };
