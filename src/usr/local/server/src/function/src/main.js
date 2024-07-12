fc12c6102f54439e20e61b0eb883ca67139c9d9a
choir27 <richardchoi54@gmail.com>
2024-07-12 10:10:06 -0400

    add content to usr/local/server/src/function/src/main.js

---
+++ src/usr/local/server/src/function/src/main.js
@@ -0,0 +1,29 @@
+// This is your Appwrite function
+// It's executed each time we get a request
+export default async ({ req, res, log, error }) => {
+    // Why not try the Appwrite SDK?
+    //
+    // const client = new Client()
+    //    .setEndpoint('https://cloud.appwrite.io/v1')
+    //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
+    //    .setKey(process.env.APPWRITE_API_KEY);
+  
+    log(res.text("test"));
+    return res.text("test");
+    // The `req` object contains the request data
+    if (req.method === 'GET') {
+      // Send a response with the res object helpers
+      // `res.send()` dispatches a string back to the client
+      log(res.text("test"));
+      return res.text('Hello, World!');
+    }
+  
+    // `res.json()` is a handy helper for sending JSON
+    return res.json({
+      motto: 'Build like a team of hundreds_',
+      learn: 'https://appwrite.io/docs',
+      connect: 'https://appwrite.io/discord',
+      getInspired: 'https://builtwith.appwrite.io',
+    });
+  };
+  
