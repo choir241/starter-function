af6a67aa911c5b57e3394cd4beccd36b6152a85d
choir27 <richardchoi54@gmail.com>
2024-07-12 11:29:22 -0400

    test

--- src/usr/local/server/src/function/src/main.js
+++ src/usr/local/server/src/function/src/main.js
@@ -5,7 +5,7 @@ export default async ({ req, res, log, error }) => {
 
  const appwriteClient = new Client()
     .setEndpoint("https://v16.appwrite.org/v1")
-    .setProject("APPWRITE_FUNCTION_PROJECT_ID")
+    .setProject("668840fe0017793b93a6")
     .setKey(req.headers["x-appwrite-key"])
 ;
 
