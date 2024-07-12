07a601661a3909670243ab27a00762f66606dc72
choir27 <richardchoi54@gmail.com>
2024-07-12 10:24:01 -0400

    final test

--- src/usr/local/server/src/function/src/main.js
+++ src/usr/local/server/src/function/src/main.js
@@ -12,6 +12,7 @@ export default async ({ req, res, log, error }) => {
     if (req.method === 'GET') {
       // Send a response with the res object helpers
       // `res.send()` dispatches a string back to the client
+      log('test')
       return res.text('Hello, World!');
     }
   
