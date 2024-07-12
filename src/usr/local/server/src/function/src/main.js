33c21a7f2ccbd0c624ed9025c4db12b8f4b73ade
choir27 <richardchoi54@gmail.com>
2024-07-12 13:10:08 -0400

    import ID from node-appwrite

--- src/usr/local/server/src/function/src/main.js
+++ src/usr/local/server/src/function/src/main.js
@@ -1,4 +1,4 @@
-import { Users, Client } from 'node-appwrite';
+import { Users, Client, ID } from 'node-appwrite';
 // This is your Appwrite function
 // It's executed each time we get a request
 export default async ({ req, res, log, error }) => {
