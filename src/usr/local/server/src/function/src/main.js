b6b04c75d030164f2f32d06cf4ed24fe97d62c9c
choir27 <richardchoi54@gmail.com>
2024-07-12 11:19:13 -0400

    update appwrite client initialiation

--- src/usr/local/server/src/function/src/main.js
+++ src/usr/local/server/src/function/src/main.js
@@ -1,9 +1,9 @@
-import { Databases } from "node-appwrite";
+import { Databases, Client } from "node-appwrite";
 // This is your Appwrite function
 // It's executed each time we get a request
 export default async ({ req, res, log, error }) => {
 
- appwriteClient = client.NewClient()
+ appwriteClient = new Client()
 	appwriteClient.SetEndpoint(os.Getenv("https://v16.appwrite.org/v1"))
 	appwriteClient.SetProject(os.Getenv("APPWRITE_FUNCTION_PROJECT_ID"))
 	appwriteClient.SetKey(Context.Req.Headers["x-appwrite-key"])
