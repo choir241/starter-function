42a71b9913e6a554d1541a4f5e5892b2b66e4201
choir27 <richardchoi54@gmail.com>
2024-07-12 10:46:21 -0400

    test bodyJson and bodyText

--- src/main.js
+++ src/main.js
@@ -1,30 +1,11 @@
 // This is your Appwrite function
 // It's executed each time we get a request
 export default async ({ req, res, log, error }) => {
-  // Why not try the Appwrite SDK?
-  //
-  // const client = new Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(process.env.APPWRITE_FUNCTION_PROJECT_ID)
-  //    .setKey(process.env.APPWRITE_API_KEY);
 
-
-  // return res.text("test");
-  // The `req` object contains the request data
   if (req.method === 'GET') {
-    // Send a response with the res object helpers
-    // `res.send()` dispatches a string back to the client
-    const headers = ''
-    log(res.text('{ success: false }', 404, headers));
-    res.text('{ success: false }', 404, headers);
-    return res.text('{ success: false }', 404, headers)
+    log(req.bodyJson);
+    log(req.bodyText)
+    return req.bodyText;
   }
 
-  // `res.json()` is a handy helper for sending JSON
-  return res.json({
-    motto: 'Build like a team of hundreds_',
-    learn: 'https://appwrite.io/docs',
-    connect: 'https://appwrite.io/discord',
-    getInspired: 'https://builtwith.appwrite.io',
-  });
 };
