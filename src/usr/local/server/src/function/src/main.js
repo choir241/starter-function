2c0c75bd5c581f23394c8602efe6d3ff5e5b0a8e
choir27 <richardchoi54@gmail.com>
2024-07-19 16:35:01 -0400

    add all response types to test their logs

--- src/usr/local/server/src/function/src/main.js
+++ src/usr/local/server/src/function/src/main.js
@@ -1,17 +1,30 @@
-import { Users, Client, ID } from 'node-appwrite';
+import { Client } from 'node-appwrite';
+
 // This is your Appwrite function
 // It's executed each time we get a request
 export default async ({ req, res, log, error }) => {
-  if (req.method === 'GET') {
-    const client = new Client()
-      .setEndpoint('https://v16.appwrite.org/v1')
-      .setProject('668840fe0017793b93a6')
-      .setKey(req.headers['x-appwrite-key']);
-    const users = new Users(client);
-    const user = await users.create(ID.unique(), 'matej@appwrite.io');
-    log(res.text(user.$id))
-    log(req.bodyText)
-    log(req.bodyJson)
-    return res.text(user.$id);
+  try{
+    if (req.method === 'POST') {
+      // const client = new Client()
+      //   .setEndpoint('https://v16.appwrite.org/v1')
+      //   .setProject('668840fe0017793b93a6')
+      //   .setKey(req.headers['x-appwrite-key']);
+        
+      // const users = new Users(client);
+      // await users.create(ID.unique(), 'test@email.com', 'testPassword', 'testName');
+      // log(req.bodyJson);
+      // log(req.bodyText);
+      log(res.send("send user was successfully created"));
+      log(res.text("text user was successfully created"));
+      log(res.binary("binary user was successfully created"));
+      log(res.json("json user was successfully created"));
+      log(res.empty("empty user was successfully created"));
+      log(res.redirect("redirect user was successfully created"));
+
+      return res.text("User was successfully created!");
+    }
+  }catch(err){
+    console.error(err);
+    log(err);
   }
 };
